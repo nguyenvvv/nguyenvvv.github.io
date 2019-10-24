e9c34bc4a624e012dfa1f9a468b3a25bb000d1d4
Vi Nguyen <nguyenvv@hendrix.edu>
2019-10-24 16:07:46 -0500

    did jquery stuff

---
+++ csci340/jsdemo/demoforjs.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#introduction").html();
+  var trafficLight1 = function(){
+    $("img").attr("src", "images/red.png");
+  }
+  var trafficLight2 = function(){
+    $("img").attr("src", "images/green.png");
+  }
+  var trafficLight3 = function(){
+    $("img").attr("src", "images/yellow.png");
+  }
+
+  $("clicker").click(trafficLight1);
+  $("clicker").click(trafficLight2);
+  $("clicker").click(trafficLight3);
+
+
+});
