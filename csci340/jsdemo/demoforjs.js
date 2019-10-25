7729335dfb341e88d4584157ff08f69c77cab917
Vi Nguyen <nguyenvv@hendrix.edu>
2019-10-24 23:37:28 -0500

    everything is fixed now

--- csci340/jsdemo/demoforjs.js
+++ csci340/jsdemo/demoforjs.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
-  $("#introduction").html();
   var trafficLight1 = function(){
+    $("#introduction").html("Red Light!");
     $("img").attr("src", "images/red.png");
-    
+    $("#clicker").click(trafficLight2);
+
   }
   var trafficLight2 = function(){
+    $("#introduction").html("Green Light!");
     $("img").attr("src", "images/green.png");
+    $("#clicker").click(trafficLight3);
   }
   var trafficLight3 = function(){
+    $("#introduction").html("Yellow Light!");
     $("img").attr("src", "images/yellow.png");
+    $("#clicker").click(trafficLight1);
   }
 
-  $("clicker").click(trafficLight1);
-  $("clicker").click(trafficLight2);
-  $("clicker").click(trafficLight3);
+  $("#clicker").click(trafficLight1);
+  //$("#clicker").click(trafficLight2);
+  //$("#clicker").click(trafficLight3);
 
 
 });
